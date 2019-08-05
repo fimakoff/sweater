05e49cdc7dc5ee23582859eccb178abe49cace12
fimakoff <fimakoff@gmail.com>
2019-08-05 16:14:34 +0300

    Add database and filter

---
+++ src/main/java/com/fimakoff/sweater/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.fimakoff.sweater.repos;
+
+import com.fimakoff.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message,Long> {
+    List<Message> findByTag(String tag);
+}
