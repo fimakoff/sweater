12777ab36fee5019324f392f61e1ee8f85bf8314
fimakoff <fimakoff@gmail.com>
2019-08-06 17:11:44 +0300

    Add user name display

--- src/main/java/com/fimakoff/sweater/controller/MainController.java
+++ src/main/java/com/fimakoff/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.fimakoff.sweater.controller;
 
 import com.fimakoff.sweater.domain.Message;
+import com.fimakoff.sweater.domain.User;
 import com.fimakoff.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,11 +30,13 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String, Object> model) {
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
 
-        Message message = new Message(text, tag);
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
