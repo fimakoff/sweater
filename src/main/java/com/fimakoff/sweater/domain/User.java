d2af6f80abfd1b6279253439a417ac4dc12173e2
fimakoff <fimakoff@gmail.com>
2019-08-10 21:16:46 +0300

    Added email sender and activation code

--- src/main/java/com/fimakoff/sweater/domain/User.java
+++ src/main/java/com/fimakoff/sweater/domain/User.java
@@ -22,6 +22,9 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    private String email;
+    private String activationCode;
+
     public boolean isAdmin(){
         return roles.contains(Role.ADMIN);
     }
@@ -89,5 +92,21 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
